1a08354cb865e1617b9a024175414abdc6c3d677
adamlass <33089759+adamlass@users.noreply.github.com>
2019-05-25 16:43:38 +0200

    adding comments and link-indicator

--- moveDetect.py
+++ moveDetect.py
@@ -18,8 +18,8 @@ def take_picture():
     # converting color output
     image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     print("picture taken!")
-    plt.imshow(image)
-    plt.show()
+    # plt.imshow(image)
+    # plt.show()
 
 def handle_inputs():
     global auto, key_frame, found_confirmed, time_found, type_found, selected, prices
@@ -59,44 +59,69 @@ def handle_inputs():
             selected += 1
             if selected > (len(prices) - 1):
                 selected = (len(prices) - 1)
-
+        
         if keyboard.is_pressed("enter"):
-            webbrowser.open(prices[selected][2])
+            if 2 < len(prices[selected]):
+                webbrowser.open(prices[selected][2])
             
             
 
 def display_content():
     global frame, f_width, f_height, type_found, prices, found_confirmed, label_height, label_width, time_found, selected
+    
+    # copying frame
     dframe = copy.copy(frame)
 
     font = cv2.FONT_HERSHEY_COMPLEX
 
     if not found_confirmed: 
         if time_found:
+            # Calculate time remaining for countdown
             counter = int((time_found + 5) - time.time())
+            
+            # If it goes under zero, accept the type found
             if counter < 0:
                 found_confirmed = True
                 time_found = None
+            # If not, display the counter
             else:
                 cv2.putText(dframe, str(counter), (int(f_width / 2) - 40,100), font, 4, (255,255,255), 5, cv2.LINE_AA)
 
+    
     if type_found:
+        # Generate type text depending on confirmation
         display_found = type_found
         indent = 100
         if not found_confirmed:
             display_found = f"Is this: {type_found}?"
             indent = 250
-        cv2.putText(dframe, display_found, (int(f_width/2) - indent,(f_height-50)), font, 2, (255,255,255), 5, cv2.LINE_AA)
 
-    y_delta = 0
+        # Displaying type text 
+        cv2.putText(dframe, display_found, (int(f_width/2) - indent, (f_height-50)), font, 2, (255,255,255), 5, cv2.LINE_AA)
+
+    # Value for relative height position 
+    height_pos = 0
     for key, price in enumerate(prices):
+        # If the current price is the selected one, display the background color as white
         label_color = (0,255,255)
         if key == selected:
             label_color = (255,255,255)
-        cv2.rectangle(dframe, (10,10 + y_delta), (200,label_height + 10 + y_delta), label_color, -1)
-        cv2.putText(dframe, price[1], (15,35 + y_delta), font, 0.7, (0,0,0), 1, cv2.LINE_AA)
-        cv2.putText(dframe, f"{price[0]} kr", (15,80 + y_delta), font, 1.4, (0,0,0), 2, cv2.LINE_AA)
-        y_delta += label_height + 10
+        
+        # Draw background
+        cv2.rectangle(dframe, (10,10 + height_pos), (200,label_height + 10 + height_pos), label_color, -1)
+
+        # Display store
+        cv2.putText(dframe, price[1], (15,35 + height_pos), font, 0.7, (0,0,0), 1, cv2.LINE_AA)
+
+        # Display price
+        cv2.putText(dframe, f"{price[0]} kr", (15,80 + height_pos), font, 1.4, (0,0,0), 1, cv2.LINE_AA)
+
+        if 2 < len(price):
+            # Display price
+            cv2.putText(dframe, ">", (label_width - 30, int(label_height / 2) + 20 + height_pos), font, 0.7, (0,0,0), 1, cv2.LINE_AA)
+
+        # Append height_pos
+        height_pos += label_height + 10
 
     return dframe
 
@@ -123,7 +148,7 @@ if __name__ == "__main__":
     type_found = "apple"
     time_found = time.time()
     found_confirmed = False
-    prices = [(3,"Kwickly", "https://kwickly.dk/"), (4, "Netto", "https://netto.dk/"), (6, "Irma", "https://irma.dk/"), (2,"Fakta", "https://fakta.dk/")]
+    prices = [(3,"Kwickly", "https://kwickly.dk/"), (4, "Netto"), (6, "Irma", "https://irma.dk/"), (2,"Fakta", "https://fakta.dk/")]
 
     # selection
     selected = 0
